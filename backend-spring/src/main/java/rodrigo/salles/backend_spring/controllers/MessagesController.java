35d14b96129b5698b260c990f77e1479231fe6be
rgsax1 <rgsa@outlook.com.br>
2024-05-24 14:40:04 -0300

    Configure mariadb connection

--- backend-spring/src/main/java/rodrigo/salles/backend_spring/controllers/MessagesController.java
+++ backend-spring/src/main/java/rodrigo/salles/backend_spring/controllers/MessagesController.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class MessagesController {
     @GetMapping("/messages")
     public ResponseEntity<List<String>> messages() {
--- frontend-react/src/components/AuthContent.jsx
+++ frontend-react/src/components/AuthContent.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { request } from '../axios_helper'
+import * as React from 'react';
 
-const AuthContent = props => {
-    constructor(props){
+import { request, setAuthHeader } from '../helpers/axios_helper';
+
+export default class AuthContent extends React.Component {
+
+    constructor(props) {
         super(props);
         this.state = {
-            data : []
-        };
+            data: []
+        }
     };
 
-    componentDidMount(){
+    componentDidMount() {
         request(
             "GET",
             "/messages",
-            {}
-        ).then((response) => {
-            this.setState({data: response.data})
-        })
-    }
+            {}).then(
+            (response) => {
+                this.setState({data: response.data})
+            }).catch(
+            (error) => {
+                if (error.response.status === 401) {
+                    setAuthHeader(null);
+                } else {
+                    this.setState({data: error.response.code})
+                }
 
+            }
+        );
+    };
 
-  return (
-    <div>AuthContent</div>
-  )
+  render() {
+    return (
+        <div className="row justify-content-md-center">
+            <div className="col-4">
+                <div className="card" style={{width: "18rem"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Backend response</h5>
+                        <p className="card-text">Content:</p>
+                        <ul>
+                            {this.state.data && this.state.data
+                                .map((line) =>
+                                    <li key={line}>{line}</li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  };
 }
-
-AuthContent.propTypes = {}
-
-export default AuthContent
