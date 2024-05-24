c94537debafb91b967ef55cecabe1ed162b6c0f2
rgsax1 <rgsa@outlook.com.br>
2024-05-24 16:08:33 -0300

    Starting Angular project

---
+++ frontend-angular/src/app/auth-content/auth-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-content.component.html',
+  styleUrl: './auth-content.component.css'
+})
+export class AuthContentComponent {
+
+}
