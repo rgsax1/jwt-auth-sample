45fe6580eaf19caa832c8ffb9ca166d421f9bff2
rgsax1 <rgsa@outlook.com.br>
2024-05-25 12:57:39 -0300

    moving files

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
-import {ContentComponent} from "./content/content.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {ContentComponent} from "./components/content/content.component";
 
 @Component({
   selector: 'app-root',
